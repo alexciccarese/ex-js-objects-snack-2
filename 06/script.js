0be52f7fa4f1964a115f464ae4054b91e25fc8bd
alexciccarese <alex.ciccarese@gmail.com>
2025-06-06 19:23:48 +0200

    Add initial HTML and JavaScript files for Snack 6 project

---
+++ 06/script.js
@@ -0,0 +1,29 @@
+// Qual è il metodo migliore per clonare l’oggetto chef, e perché? Spread operator
+
+const chef = {
+	name: "Chef Hyur",
+	age: 29,
+	makeBurger: (num = 1) => {
+		console.log(`Ecco ${num} hamburger per te!`);
+	},
+	restaurant: {
+		name: "Hyur's Burgers",
+		welcomeClient: () => {
+			console.log("Benvenuto!");
+		},
+		address: {
+			street: 'Main Street',
+			number: 123,
+			showAddress: () => {
+				console.log("Main Street 123");
+			}
+		},
+		isOpen: true,
+	} 
+}
+
+const newChef = {...chef}
+
+console.log(chef);
+console.log(newChef);
+
