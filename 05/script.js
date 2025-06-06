ee0de21180dfd6dad9531e7d950e6f59465abb71
alexciccarese <alex.ciccarese@gmail.com>
2025-06-06 19:14:22 +0200

    Add initial HTML and JavaScript files for Snack 5 project

---
+++ 05/script.js
@@ -0,0 +1,28 @@
+// Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
+// Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice? 5
+
+const hamburger = { 
+	name: "Cheese Burger", 
+	weight: 250,
+	maker: {
+		name: "Anonymous Chef",
+		restaurant: {
+			name: "Hyur's Burgers",
+			address: "Main Street, 123",
+			isOpen: true,
+		},
+		age: 29
+	}
+};
+
+const newRestaurant = {...hamburger.maker.restaurant};
+newRestaurant.name = "Hyur's II";
+newRestaurant.address = "Second Street, 12";
+const secondBurger = {...hamburger};
+secondBurger.maker.restaurant = newRestaurant;
+secondBurger.maker.name = "Chef Hyur";
+
+console.log(hamburger.maker.name); // "Chef Hyur"
+console.log(secondBurger.maker.name); // "Chef Hyur"
+console.log(hamburger.maker.restaurant.name); // "Hyur's II"
+console.log(secondBurger.maker.restaurant.name); // "Hyur's II"
