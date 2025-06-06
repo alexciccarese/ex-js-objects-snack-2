57bdb98bb5c665aede3410610be798d7898210ef
alexciccarese <alex.ciccarese@gmail.com>
2025-06-06 12:49:00 +0200

    Update comments and console output in script.js for clarity

--- 02/script.js
+++ 02/script.js
@@ -1,5 +1,5 @@
-// Senza lanciare il codice, riesci a prevedere cosa viene stampato in console? "Cheese" - "Salad"
-// Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice? 2
+// Senza lanciare il codice, riesci a prevedere cosa viene stampato in console? "Salad"
+// Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice? 3
 
 const hamburger = { 
 	name: "Cheese Burger", 
@@ -10,5 +10,5 @@ const hamburger = {
 const secondBurger = {...hamburger};
 secondBurger.ingredients[0] = "Salad";
 
-console.log(hamburger.ingredients[0]); // "Cheese"
+console.log(hamburger.ingredients[0]); // "Salad"
 console.log(secondBurger.ingredients[0]); // "Salad"
